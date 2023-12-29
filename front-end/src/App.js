fa70cbc55539a0d7ebd87d5ce1357def1432ea03
kubabukry <95041821+kubabukry@users.noreply.github.com>
2023-12-29 23:38:10 +0100

    jwt stored in localStorage

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,33 @@
 import './App.css';
+import React from 'react';
+import { useLocalState } from './util/useLocalStorage';
 
 function App() {
+  const [jwt, setJwt] = useLocalState("", "jwt");
 
-  fetch('http://localhost:8080/home', {
-    headers: {
-    },
-    method: "get"
-
-  });
+  React.useEffect(() => {
+    const reqBody = {
+      login: "user",
+      password: "P@ssw0rd"
+    };
+    
+    if(!jwt){
+      fetch('http://localhost:8080/person/auth/authenticate', {
+        headers: {
+          "Content-Type": "application/json"
+        },
+        method: "post",
+        body: JSON.stringify(reqBody),
+      })
+      .then(response => response.json())
+      .then(json => setJwt(json.token));
+    }
+  }, []);
 
   return (
     <div className="App">
      <h1>Hello world!</h1>
+     <div>JWT value: {jwt}</div>
     </div>
   );
 }
--- src/main/java/com/pl/edu/wieik/flightScheduler/configuration/SecurityConfig.java
+++ src/main/java/com/pl/edu/wieik/flightScheduler/configuration/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
 
 @Configuration
@@ -42,6 +43,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
